c03aedb4ef47f24e11d3eda4824e60566f1ae0c5
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2024-02-05 13:31:22 -0500

    laboratorio manejo de hooks en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,50 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-hooks';
+export class AppComponent  {
+
+  counter: number;
+
+  constructor() {
+      console.log('constructor');     
+      this.counter = 1;
+  }
+
+  inc(){
+      console.log('inc');
+      this.counter += 1;
+  }
+
+  ngOnInit(){
+      console.log('ng on init');
+  }
+
+  ngOnDestroy(){
+      console.log('ngOnDestroy');
+  }
+
+  ngDoCheck(){
+      console.log('ngDoCheck');
+  }
+
+  ngOnChanges(){
+      console.log('ngOnChanges');
+  }
+
+  ngAfterContentInit(){
+      console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+      console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(){
+      console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(){
+      console.log('ngAfterViewChecked');
+  }
 }
+
+
